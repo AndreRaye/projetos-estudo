3c60eecfead749918168fcc12f6d337276901407
André Raye <103725287+AndreRaye@users.noreply.github.com>
2022-05-12 16:06:33 -0400

    contador de passageiros

---
+++ contador-passageiros/index.js
@@ -0,0 +1,19 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count   
+}
+
+function subtrair() {
+    count -= 1
+    countEl.innerText = count
+}
+
+function save() {
+    saveEl.textContent += " " + count + " - "
+    count = 0
+    countEl.innerText = count
+}
---
+++ contador-passageiros/readme.txt
@@ -0,0 +1,6 @@
+Este é um app de contagem de passageiros utilizando JavaScript, HTML e CSS.
+
+A ideia é que:
+A cada passageiro contado, você adicione ele no app usando o botão "ADICIONAR"
+Caso erre na contagem use o botão "SUBTRAIR"
+Para salvar a contagem atual e resetar o contador utilize o botão "SALVAR"
